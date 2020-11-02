c6c95612cd5258f2ca7596899416c8ba971e007c
zhang <494089941@qq.com>
2020-11-02 09:27:44 +0800

    系统消息页

--- lib/style/app_style.dart
+++ lib/style/app_style.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 class AppStyle {
   //颜色，渐变主颜色和次级颜色
   static const primaryColor =Color(0xFFFDCF12);
   static const minorColor=Color(0xFFFFDF5D);
 
-  //字体
+  //字体颜色
   static const primaryTextColor=Color(0xFF333333);
   static const minorTextColor=Color(0xFF999999);
+
+  //字体格式
+  final barTitleStyle=TextStyle(
+    color: primaryTextColor,
+    fontSize: 36.sp,
+    fontWeight: FontWeight.bold,
+  );
 
 }
