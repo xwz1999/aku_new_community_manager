8c819352ad70217016907c664b20724d7163d6fc
laiiihz <laiiihz@outlook.com>
2020-11-03 09:06:35 +0800

    添加访客管理页面

---
+++ lib/ui/sub_pages/activity_manager/visitor_manager/visitor_manager_card.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class VisitorManagerCard extends StatefulWidget {
+  VisitorManagerCard({Key key}) : super(key: key);
+
+  @override
+  _VisitorManagerCardState createState() => _VisitorManagerCardState();
+}
+
+class _VisitorManagerCardState extends State<VisitorManagerCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //  child: child,
+    );
+  }
+}
---
+++ lib/ui/sub_pages/activity_manager/visitor_manager/visitor_manager_page.dart
@@ -0,0 +1,20 @@
+import 'package:aku_community_manager/ui/widgets/common/aku_scaffold.dart';
+import 'package:flutter/material.dart';
+
+class VisitorManagerPage extends StatefulWidget {
+  VisitorManagerPage({Key key}) : super(key: key);
+
+  @override
+  _VisitorManagerPageState createState() => _VisitorManagerPageState();
+}
+
+class _VisitorManagerPageState extends State<VisitorManagerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return AkuScaffold(
+        // title: '',
+        // appBarBottom: ,
+        // body: ,
+        );
+  }
+}
