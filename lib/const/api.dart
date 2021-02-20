75240997323bf55080c057862ba570212e7c40b9
zhang <494089941@qq.com>
2021-02-20 14:39:55 +0800

    对接：修改昵称

--- lib/const/api.dart
+++ lib/const/api.dart
@@ -32,4 +32,7 @@ class _User {
 
   ///个人资料：获取用户信息
   String get info => '/user/personalData/getUserDetail';
+
+  ///个人资料：修改管家用户昵称
+  String get updateNickName => '/user/personalData/updateNickName';
 }
