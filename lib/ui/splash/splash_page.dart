54789087be6f5b1b55a17953808d8df7e08c200f
zhangmeng <494089941@qq.com>
2021-04-14 13:40:30 +0800

    更换高德地图插件为官方插件

--- lib/ui/splash/splash_page.dart
+++ lib/ui/splash/splash_page.dart
@@ -1,6 +1,7 @@
 // Flutter imports:
 import 'package:aku_community_manager/const/api.dart';
 import 'package:aku_community_manager/models/manager/item_num_model.dart';
+import 'package:aku_community_manager/provider/app_provider.dart';
 import 'package:aku_community_manager/provider/user_provider.dart';
 import 'package:aku_community_manager/style/app_style.dart';
 import 'package:aku_community_manager/utils/dev_util.dart';
@@ -9,11 +10,11 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 // Package imports:
-import 'package:amap_map_fluttify/amap_map_fluttify.dart';
 import 'package:get/get.dart' hide Response;
 import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:jpush_flutter/jpush_flutter.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:power_logger/power_logger.dart';
 
 // Project imports:
@@ -39,7 +40,14 @@ class _SplashPageState extends State<SplashPage> {
     }
 
     //初始化AMap
-    await AmapLocation.instance.init(iosKey: 'ios key');
+    // await AmapLocation.instance.init(iosKey: 'ios key');
+    await Permission.locationWhenInUse.request();
+  }
+
+  Future _initOp() async {
+    await _originOp();
+    final appProvider = Provider.of<AppProvider>(context, listen: false);
+    appProvider.startLocation();
   }
 
   @override
@@ -49,7 +57,7 @@ class _SplashPageState extends State<SplashPage> {
       if (mounted) PowerLogger.start(context, debug: DevUtil.isDev);
     });
     Future.delayed(Duration(milliseconds: 2000), () async {
-      await _originOp();
+      await _initOp();
       Get.off(HomePage());
     });
   }
