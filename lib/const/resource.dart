56c98ae5291ea1a7b553b9f1f8b12ddbf1378441
zhangmeng <494089941@qq.com>
2021-08-17 15:10:11 +0800

    delete git ignore /resource.dart

---
+++ lib/const/resource.dart
@@ -0,0 +1,349 @@
+/// Generate by [resource_generator](https://github.com/CaiJingLong/flutter_resource_generator) library.
+/// PLEASE DO NOT EDIT MANUALLY.
+class R {
+  const R._();
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/clock_in_out.png)
+  static const String ASSETS_HOME_CLOCK_IN_OUT_PNG =
+      'assets/home/clock_in_out.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/facilities.png)
+  static const String ASSETS_HOME_FACILITIES_PNG = 'assets/home/facilities.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/house_keeping.png)
+  static const String ASSETS_HOME_HOUSE_KEEPING_PNG =
+      'assets/home/house_keeping.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/hygienice.png)
+  static const String ASSETS_HOME_HYGIENICE_PNG = 'assets/home/hygienice.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/ic_Patrol.png)
+  static const String ASSETS_HOME_IC_PATROL_PNG = 'assets/home/ic_Patrol.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/ic_activity.png)
+  static const String ASSETS_HOME_IC_ACTIVITY_PNG =
+      'assets/home/ic_activity.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/ic_all.png)
+  static const String ASSETS_HOME_IC_ALL_PNG = 'assets/home/ic_all.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/ic_article.png)
+  static const String ASSETS_HOME_IC_ARTICLE_PNG = 'assets/home/ic_article.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/ic_borrow.png)
+  static const String ASSETS_HOME_IC_BORROW_PNG = 'assets/home/ic_borrow.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/ic_decoration.png)
+  static const String ASSETS_HOME_IC_DECORATION_PNG =
+      'assets/home/ic_decoration.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/ic_facilities.png)
+  static const String ASSETS_HOME_IC_FACILITIES_PNG =
+      'assets/home/ic_facilities.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/ic_greening.png)
+  static const String ASSETS_HOME_IC_GREENING_PNG =
+      'assets/home/ic_greening.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/ic_news.png)
+  static const String ASSETS_HOME_IC_NEWS_PNG = 'assets/home/ic_news.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/ic_police.png)
+  static const String ASSETS_HOME_IC_POLICE_PNG = 'assets/home/ic_police.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/ic_scan.png)
+  static const String ASSETS_HOME_IC_SCAN_PNG = 'assets/home/ic_scan.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/ic_search.png)
+  static const String ASSETS_HOME_IC_SEARCH_PNG = 'assets/home/ic_search.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/ic_service.png)
+  static const String ASSETS_HOME_IC_SERVICE_PNG = 'assets/home/ic_service.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/ic_visitors.png)
+  static const String ASSETS_HOME_IC_VISITORS_PNG =
+      'assets/home/ic_visitors.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/interview.png)
+  static const String ASSETS_HOME_INTERVIEW_PNG = 'assets/home/interview.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/key.png)
+  static const String ASSETS_HOME_KEY_PNG = 'assets/home/key.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/package.png)
+  static const String ASSETS_HOME_PACKAGE_PNG = 'assets/home/package.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/home/rules.png)
+  static const String ASSETS_HOME_RULES_PNG = 'assets/home/rules.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/inspection/ic_date.png)
+  static const String ASSETS_INSPECTION_IC_DATE_PNG =
+      'assets/inspection/ic_date.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/inspection/ic_inspection_name.png)
+  static const String ASSETS_INSPECTION_IC_INSPECTION_NAME_PNG =
+      'assets/inspection/ic_inspection_name.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/inspection/ic_pattern.png)
+  static const String ASSETS_INSPECTION_IC_PATTERN_PNG =
+      'assets/inspection/ic_pattern.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/inspection/ic_station.png)
+  static const String ASSETS_INSPECTION_IC_STATION_PNG =
+      'assets/inspection/ic_station.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/inspection/ic_way.png)
+  static const String ASSETS_INSPECTION_IC_WAY_PNG =
+      'assets/inspection/ic_way.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/inspection/ic_xunjian.png)
+  static const String ASSETS_INSPECTION_IC_XUNJIAN_PNG =
+      'assets/inspection/ic_xunjian.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/inspection/inspection_station.png)
+  static const String ASSETS_INSPECTION_INSPECTION_STATION_PNG =
+      'assets/inspection/inspection_station.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/add.png)
+  static const String ASSETS_MANAGE_ADD_PNG = 'assets/manage/add.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/address.png)
+  static const String ASSETS_MANAGE_ADDRESS_PNG = 'assets/manage/address.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/article.png)
+  static const String ASSETS_MANAGE_ARTICLE_PNG = 'assets/manage/article.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/borrow.png)
+  static const String ASSETS_MANAGE_BORROW_PNG = 'assets/manage/borrow.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/check.png)
+  static const String ASSETS_MANAGE_CHECK_PNG = 'assets/manage/check.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/circuit.png)
+  static const String ASSETS_MANAGE_CIRCUIT_PNG = 'assets/manage/circuit.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/clock.png)
+  static const String ASSETS_MANAGE_CLOCK_PNG = 'assets/manage/clock.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/decoration.png)
+  static const String ASSETS_MANAGE_DECORATION_PNG =
+      'assets/manage/decoration.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/dingwei.png)
+  static const String ASSETS_MANAGE_DINGWEI_PNG = 'assets/manage/dingwei.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/estate.png)
+  static const String ASSETS_MANAGE_ESTATE_PNG = 'assets/manage/estate.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/home.png)
+  static const String ASSETS_MANAGE_HOME_PNG = 'assets/manage/home.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/ic_announce.png)
+  static const String ASSETS_MANAGE_IC_ANNOUNCE_PNG =
+      'assets/manage/ic_announce.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/ic_car.png)
+  static const String ASSETS_MANAGE_IC_CAR_PNG = 'assets/manage/ic_car.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/ic_daofang.png)
+  static const String ASSETS_MANAGE_IC_DAOFANG_PNG =
+      'assets/manage/ic_daofang.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/ic_guoqi.png)
+  static const String ASSETS_MANAGE_IC_GUOQI_PNG = 'assets/manage/ic_guoqi.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/ic_image.png)
+  static const String ASSETS_MANAGE_IC_IMAGE_PNG = 'assets/manage/ic_image.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/ic_renwu.png)
+  static const String ASSETS_MANAGE_IC_RENWU_PNG = 'assets/manage/ic_renwu.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/ic_time.png)
+  static const String ASSETS_MANAGE_IC_TIME_PNG = 'assets/manage/ic_time.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/ic_wancheng.png)
+  static const String ASSETS_MANAGE_IC_WANCHENG_PNG =
+      'assets/manage/ic_wancheng.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/ic_weidao.png)
+  static const String ASSETS_MANAGE_IC_WEIDAO_PNG =
+      'assets/manage/ic_weidao.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/ic_zhuangxiu.png)
+  static const String ASSETS_MANAGE_IC_ZHUANGXIU_PNG =
+      'assets/manage/ic_zhuangxiu.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/info.png)
+  static const String ASSETS_MANAGE_INFO_PNG = 'assets/manage/info.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/key.png)
+  static const String ASSETS_MANAGE_KEY_PNG = 'assets/manage/key.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/location.png)
+  static const String ASSETS_MANAGE_LOCATION_PNG = 'assets/manage/location.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/lock.png)
+  static const String ASSETS_MANAGE_LOCK_PNG = 'assets/manage/lock.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/police.png)
+  static const String ASSETS_MANAGE_POLICE_PNG = 'assets/manage/police.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/remaining.png)
+  static const String ASSETS_MANAGE_REMAINING_PNG =
+      'assets/manage/remaining.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/security.png)
+  static const String ASSETS_MANAGE_SECURITY_PNG = 'assets/manage/security.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/status.png)
+  static const String ASSETS_MANAGE_STATUS_PNG = 'assets/manage/status.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/submit_success.png)
+  static const String ASSETS_MANAGE_SUBMIT_SUCCESS_PNG =
+      'assets/manage/submit_success.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/wall.png)
+  static const String ASSETS_MANAGE_WALL_PNG = 'assets/manage/wall.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/warning.png)
+  static const String ASSETS_MANAGE_WARNING_PNG = 'assets/manage/warning.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/waterway.png)
+  static const String ASSETS_MANAGE_WATERWAY_PNG = 'assets/manage/waterway.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/manage/window.png)
+  static const String ASSETS_MANAGE_WINDOW_PNG = 'assets/manage/window.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/message/ic_area.png)
+  static const String ASSETS_MESSAGE_IC_AREA_PNG = 'assets/message/ic_area.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/message/ic_people.png)
+  static const String ASSETS_MESSAGE_IC_PEOPLE_PNG =
+      'assets/message/ic_people.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/message/ic_phone.png)
+  static const String ASSETS_MESSAGE_IC_PHONE_PNG =
+      'assets/message/ic_phone.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/message/ic_pinglun.png)
+  static const String ASSETS_MESSAGE_IC_PINGLUN_PNG =
+      'assets/message/ic_pinglun.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/message/ic_star.png)
+  static const String ASSETS_MESSAGE_IC_STAR_PNG = 'assets/message/ic_star.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/message/ic_tongzhi.png)
+  static const String ASSETS_MESSAGE_IC_TONGZHI_PNG =
+      'assets/message/ic_tongzhi.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/outdoor/ic_address.png)
+  static const String ASSETS_OUTDOOR_IC_ADDRESS_PNG =
+      'assets/outdoor/ic_address.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/outdoor/ic_chuhu.png)
+  static const String ASSETS_OUTDOOR_IC_CHUHU_PNG =
+      'assets/outdoor/ic_chuhu.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/outdoor/ic_goout.png)
+  static const String ASSETS_OUTDOOR_IC_GOOUT_PNG =
+      'assets/outdoor/ic_goout.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/outdoor/ic_home.png)
+  static const String ASSETS_OUTDOOR_IC_HOME_PNG = 'assets/outdoor/ic_home.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/outdoor/ic_image.png)
+  static const String ASSETS_OUTDOOR_IC_IMAGE_PNG =
+      'assets/outdoor/ic_image.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/outdoor/ic_people.png)
+  static const String ASSETS_OUTDOOR_IC_PEOPLE_PNG =
+      'assets/outdoor/ic_people.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/outdoor/ic_phoneblue.png)
+  static const String ASSETS_OUTDOOR_IC_PHONEBLUE_PNG =
+      'assets/outdoor/ic_phoneblue.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/outdoor/ic_time.png)
+  static const String ASSETS_OUTDOOR_IC_TIME_PNG = 'assets/outdoor/ic_time.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/outdoor/ic_transport.png)
+  static const String ASSETS_OUTDOOR_IC_TRANSPORT_PNG =
+      'assets/outdoor/ic_transport.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/outdoor/ic_weight.png)
+  static const String ASSETS_OUTDOOR_IC_WEIGHT_PNG =
+      'assets/outdoor/ic_weight.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/outdoor/item2.png)
+  static const String ASSETS_OUTDOOR_ITEM2_PNG = 'assets/outdoor/item2.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/outdoor/item3.png)
+  static const String ASSETS_OUTDOOR_ITEM3_PNG = 'assets/outdoor/item3.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/outdoor/itme1.png)
+  static const String ASSETS_OUTDOOR_ITME1_PNG = 'assets/outdoor/itme1.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/placeholder.webp)
+  static const String ASSETS_PLACEHOLDER_WEBP = 'assets/placeholder.webp';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/static_fix/air1.png)
+  static const String ASSETS_STATIC_FIX_AIR1_PNG = 'assets/static_fix/air1.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/static_fix/air2.png)
+  static const String ASSETS_STATIC_FIX_AIR2_PNG = 'assets/static_fix/air2.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/static_fix/food.png)
+  static const String ASSETS_STATIC_FIX_FOOD_PNG = 'assets/static_fix/food.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/static_fix/food2.png)
+  static const String ASSETS_STATIC_FIX_FOOD2_PNG =
+      'assets/static_fix/food2.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/static_fix/light1.png)
+  static const String ASSETS_STATIC_FIX_LIGHT1_PNG =
+      'assets/static_fix/light1.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/static_fix/light2.png)
+  static const String ASSETS_STATIC_FIX_LIGHT2_PNG =
+      'assets/static_fix/light2.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/static_fix/light3.png)
+  static const String ASSETS_STATIC_FIX_LIGHT3_PNG =
+      'assets/static_fix/light3.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/static_temp/drill.png)
+  static const String ASSETS_STATIC_TEMP_DRILL_PNG =
+      'assets/static_temp/drill.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/static_temp/f1.webp)
+  static const String ASSETS_STATIC_TEMP_F1_WEBP = 'assets/static_temp/f1.webp';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/static_temp/f2.webp)
+  static const String ASSETS_STATIC_TEMP_F2_WEBP = 'assets/static_temp/f2.webp';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/static_temp/f3.webp)
+  static const String ASSETS_STATIC_TEMP_F3_WEBP = 'assets/static_temp/f3.webp';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/static_temp/f4.webp)
+  static const String ASSETS_STATIC_TEMP_F4_WEBP = 'assets/static_temp/f4.webp';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/static_temp/f5.webp)
+  static const String ASSETS_STATIC_TEMP_F5_WEBP = 'assets/static_temp/f5.webp';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/static_temp/hammer.png)
+  static const String ASSETS_STATIC_TEMP_HAMMER_PNG =
+      'assets/static_temp/hammer.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/static_temp/ladder.png)
+  static const String ASSETS_STATIC_TEMP_LADDER_PNG =
+      'assets/static_temp/ladder.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/user/ic_kefu.png)
+  static const String ASSETS_USER_IC_KEFU_PNG = 'assets/user/ic_kefu.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/user/ic_person.png)
+  static const String ASSETS_USER_IC_PERSON_PNG = 'assets/user/ic_person.png';
+
+  /// ![preview](file:///Users/zhangmeng/aku_community_manager/assets/user/ic_setup.png)
+  static const String ASSETS_USER_IC_SETUP_PNG = 'assets/user/ic_setup.png';
+}
