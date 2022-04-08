21b431685fae26c6f6234f6d87402de6840b9591
zhangmeng <494089941@qq.com>
2022-04-08 20:18:32 +0800

    fix bugs

--- lib/new_ui/new_home/application_util.dart
+++ lib/new_ui/new_home/application_util.dart
@@ -1,13 +1,6 @@
 import 'dart:ui';
 
-import 'package:aku_new_community_manager/gen/assets.gen.dart';
-import 'package:aku_new_community_manager/new_ui/household_audit/household_audit_page.dart';
-import 'package:aku_new_community_manager/new_ui/work_order/distributor/work_order_distributor_page.dart';
-import 'package:aku_new_community_manager/new_ui/work_order/receiver/work_order_receiver_page.dart';
-import 'package:aku_new_community_manager/tools/user_tool.dart';
-import 'package:aku_new_community_manager/ui/manage_pages/inspection_manage/inspection_manage_page.dart';
 import 'package:equatable/equatable.dart';
-import 'package:get/get.dart';
 
 class ApplicationUtil {
   ApplicationUtil(this.titles);
@@ -33,31 +26,7 @@ class ApplicationUtil {
     }
   }
 
-  List<AppElement> get allApplications => [
-        AppElement(
-            title: '巡检管理',
-            imgPath: Assets.home.icPatrol.path,
-            onTap: () async {
-              await Get.to(() => InspectionManagePage());
-            }),
-        AppElement(
-            title: '工单管理',
-            imgPath: Assets.home.icArticle.path,
-            onTap: () async {
-              if (UserTool.userProvider.userInfoModel!.workOrderJurisdiction ==
-                  1) {
-                Get.to(() => WorkOrderDistributorPage());
-              } else {
-                Get.to(() => WorkOrderReceiverPage());
-              }
-            }),
-        AppElement(
-            title: '住户审核',
-            imgPath: Assets.home.icVisitors.path,
-            onTap: () async {
-              Get.to(() => HouseholdAuditPage());
-            })
-      ];
+  List<AppElement> get allApplications => [];
 }
 
 class AppElement extends Equatable {
@@ -65,11 +34,11 @@ class AppElement extends Equatable {
   final String imgPath;
   final VoidCallback onTap;
 
-  const AppElement({
-    required this.title,
-    required this.imgPath,
-    required this.onTap,
-  });
+  const AppElement(
+    this.title,
+    this.imgPath,
+    this.onTap,
+  );
 
   @override
   List<Object?> get props => [
